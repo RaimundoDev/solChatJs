c56b6315398d1ad59edbd37c3609bbea04498c5d
Raimundo Nonato <raimundo.nonatotj18@outlook.com>
2019-07-23 16:22:37 -0300

    add readme.md

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-let SHOW_MESSAGE = document.getElementById('show-message')
-let TXT_MESSAGE = document.getElementById('txtMsg')
+const SHOW_MESSAGE = document.getElementById('show-message')
+const TXT_MESSAGE = document.getElementById('txtMsg')
 let BOX_COLOR = 2// 1 = light-color 2 = dark-color
-let BTN_SEND = document.getElementById('btnSend')
+const BTN_SEND = document.getElementById('btnSend')
 
 BTN_SEND.addEventListener('click', messageValidation)
 TXT_MESSAGE.addEventListener('keydown', enterSendMessage)
 
-function enterSendMessage(event) {
-    let button = event.key
+function enterSendMessage (event) {
+  const button = event.key
 
-    if (button == 'Enter') {
-        event.preventDefault()
-        messageValidation()
-    }
+  if (button === 'Enter') {
+    event.preventDefault()
+    messageValidation()
+  }
 }
 
-function messageValidation() {
-    if (TXT_MESSAGE.value == '' || TXT_MESSAGE.length == 0 || TXT_MESSAGE.value == ' ') {
-        TXT_MESSAGE.focus()
-        return false
-    } else {
-        sendTextMessage()
-    }
-}
-
-function sendTextMessage() {
-    let txtmsg = TXT_MESSAGE.value
-    
-    createBoxMessage(txtmsg)
-    cleanerCreation()
-    cleanTextBox(txtmsg)
-}
-
-function createBoxMessage(textMessage = '[ERROR]')  {
-    let newBox = document.createElement('div')
-    
-    if (BOX_COLOR == 1) {
-        newBox.setAttribute('class', 'message dark-box round-border box-shadow')
-        BOX_COLOR = 2
-    } else {
-        newBox.setAttribute('class', 'message light-box round-border box-shadow')
-        BOX_COLOR = 1
-    }
-
-    newBox.innerHTML = `<p>${textMessage}</p>`
-
-    SHOW_MESSAGE.appendChild(newBox)
-    newBox.scrollIntoView(true)
-}
-
-function cleanerCreation() {
-    let cleaner = document.createElement('div')
-    cleaner.setAttribute('class', 'cleaner')
-
-    SHOW_MESSAGE.appendChild(cleaner)
-}
-
-function cleanTextBox(textMessage) {
-    TXT_MESSAGE.value = ''
+function messageValidation () {
+  if (TXT_MESSAGE.value === '' || TXT_MESSAGE.length === 0 || TXT_MESSAGE.value === ' ') {
     TXT_MESSAGE.focus()
+    return false
+  } else {
+    sendTextMessage()
+  }
+}
+
+function sendTextMessage () {
+  const txtmsg = TXT_MESSAGE.value
+
+  createBoxMessage(txtmsg)
+  cleanerCreation()
+  cleanTextBox(txtmsg)
+}
+
+function createBoxMessage (textMessage = '[ERROR]') {
+  const newBox = document.createElement('div')
+
+  if (BOX_COLOR === 1) {
+    newBox.setAttribute('class', 'message dark-box round-border box-shadow')
+    BOX_COLOR = 2
+  } else {
+    newBox.setAttribute('class', 'message light-box round-border box-shadow')
+    BOX_COLOR = 1
+  }
+
+  newBox.innerHTML = `<p>${textMessage}</p>`
+
+  SHOW_MESSAGE.appendChild(newBox)
+  newBox.scrollIntoView(true)
+}
+
+function cleanerCreation () {
+  const cleaner = document.createElement('div')
+  cleaner.setAttribute('class', 'cleaner')
+
+  SHOW_MESSAGE.appendChild(cleaner)
+}
+
+function cleanTextBox (textMessage) {
+  TXT_MESSAGE.value = ''
+  TXT_MESSAGE.focus()
 }
